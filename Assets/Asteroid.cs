c58de5cdb35bc51a836ddc43fd65f101f08ffc8e
adf5051 <adf5051@rit.edu>
2015-09-30 14:48:13 -0400

    added ship and asteroids to project. Still rough, no game logic yet.

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+    public delegate void DestroyedDelegate(bool byPlayer);
+    public event DestroyedDelegate onDestroyed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(transform.position.z < -15)
+        {
+            if(onDestroyed != null)
+            {
+                onDestroyed(false);
+                GameObject.Destroy(gameObject);
+            }
+        }
+
+        transform.position -= new Vector3(0, 0, 5);
+    }
+}
---
+++ Assets/AsteroidEmitter.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidEmitter : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] asteroidsPrefabs = new GameObject[4];
+
+    private List<Asteroid> activeAsteroids;
+
+    private float timer = 5;
+
+	// Use this for initialization
+	void Start () {
+        activeAsteroids = new List<Asteroid>();
+	}
+
+    void OnEnable()
+    {
+        StartCoroutine(EmitterRoutine());
+    }
+	
+    void OnDisable()
+    {
+        StopCoroutine(EmitterRoutine());
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private IEnumerator EmitterRoutine()
+    {
+        while (enabled)
+        {
+            yield return new WaitForSeconds(timer);
+            GameObject temp = GameObject.Instantiate(asteroidsPrefabs[0]);
+            temp.transform.position = transform.position;
+            activeAsteroids.Add(temp.GetComponent<Asteroid>());
+        }
+    }
+}
